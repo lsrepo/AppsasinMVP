b17cc04dcf1ab9c3d05cd147e0698c8af9bc4761
Catherine Hedler <catherine.hedler@gmail.com>
2016-01-23 16:12:48 +0100

    Clean up files in folder

--- Appsassin0.1 2/Pods/Target Support Files/Pods-Appsassin0.1/Pods-Appsassin0.1-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Appsassin0_1 : NSObject
-@end
-@implementation PodsDummy_Pods_Appsassin0_1
-@end
--- Appsassin0.1 2/Pods/Target Support Files/Pods-Appsassin0.1/Pods-Appsassin0.1-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Appsassin0_1VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Appsassin0_1VersionString[];
-
