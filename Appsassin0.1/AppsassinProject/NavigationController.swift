c86f73dd4174d04552118c535e79671d52359e2a
Catherine Hedler <catherine.hedler@gmail.com>
2016-01-23 14:56:34 +0100

    clean up timevc

--- Appsassin0.1/AppsassinProject/NavigationController.swift
+++ Appsassin0.1/AppsassinProject/NavigationController.swift
@@ -13,35 +13,16 @@ class NavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Status bar white font
-        //self.navigationBar.barStyle = UIBarStyle.Black
-        //self.navigationBar.tintColor = UIColor.blackColor()
-        
-        // Navigation bar
         UINavigationBar.appearance().barTintColor = UIColor(red: 255.0/255.0, green: 215.0/255.0, blue: 0/255.0, alpha: 1.0)
-        //UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.blackColor()]
+
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33/255.0, alpha: 1.0)]
-        
-        // Customizing the tab bar
-        //UITabBar.appearance().tintColor = UIColor(red: 33.0/255.0, green: 33.0/255.0, blue: 33/255.0, alpha: 1.0)
+
         UITabBar.appearance().tintColor = UIColor(red: 255.0/255.0, green: 215.0/255.0, blue: 0/255.0, alpha: 1.0)
 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
